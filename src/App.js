a698e68da2264bb2ca920240b9f8726e71d88af9
Re04nan <renan04_marques@hotmail.com>
2021-02-05 17:40:07 -0300

    Side Server Rendering - SRR

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Copyright from './Copyright'
+
+const App = () => {
+      return (
+            <Copyright ano="2021" />
+    )
+}
+export default App
---
+++ src/Copyright.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Copyright = (props) => {
+    return <h1>Recode Pro {props.ano}!</h1>
+}
+
+export default Copyright
